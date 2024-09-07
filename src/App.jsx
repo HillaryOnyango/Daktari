f53668494129ad60d8030f50985efb64937a1c78
Hillary Onyango <hillaryonyango19@gmail.com>
2024-09-07 18:28:41 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
-  return <div>Testing</div>;
-}
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
